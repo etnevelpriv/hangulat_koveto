f33732916194fdfce1b2bd4c2731524c989ad357
etnevelpriv <leventerastovits@gmail.com>
2026-09-08 14:20:57 +0200

    index.html elkeszult

--- src/scripts/kezdooldal.ts
+++ src/scripts/kezdooldal.ts
@@ -1,20 +1,33 @@
 import { Hangulat } from "./models/Hangulat_class";
 import { Create_Hangulat } from "./services/Hangulat_CRUD";
+import { putModalText, showModal, hideModal } from "./unit/modal";
 const readFormInputs = function () {
     const hangulatElement = document.querySelector('input[name="hangulat"]:checked') as HTMLInputElement;
     const leirasElement = document.getElementById("leiras") as HTMLInputElement;
-    if (hangulatElement) {
+    const leiras = leirasElement.value;
+    if (hangulatElement && leiras) {
         return {
-            hangulat: hangulatElement.id[8],
-            leiras: leirasElement.value
+            hangulat: hangulatElement.value,
+            leiras: leiras
         };
-    }
-    showModa
+    } else {
+        putModalText("Kérlek előbb töltsd ki az összes mezőt");
+        showModal();
+        return false;
+    };
 };
-
 document.getElementById("postHangulatButton")?.addEventListener("click", async () => {
     const inputValues = readFormInputs();
-    const hangulat = new Hangulat(Number(inputValues.hangulat), inputValues.leiras, new Date());
-    const data = await Create_Hangulat(hangulat.hangulat, hangulat.szoveges_leiras, hangulat.datum);
-    console.log(data)
+    if (inputValues) {
+        console.log(inputValues)
+        const hangulat = new Hangulat(Number(inputValues.hangulat), inputValues.leiras, new Date());
+        const data = await Create_Hangulat(hangulat.hangulat, hangulat.szoveges_leiras, hangulat.datum);
+        console.log(data);
+        putModalText("Sikeres hangulat feltöltés!")
+        showModal();
+    };
+});
+document.getElementById("closeModalButton")?.addEventListener("click", () => {
+    hideModal();
+    putModalText("");
 });
--- src/scripts/unit/modal.ts
+++ src/scripts/unit/modal.ts
@@ -1,3 +1,18 @@
-const showModal = function () {
-    document.getElementById
-}
+export const showModal = function () {
+    const modalElement = document.getElementById("modal");
+    if (modalElement?.classList.contains("hide")) {
+        modalElement?.classList.remove("hide");
+    };
+};
+export const hideModal = function () {
+    const modalElement = document.getElementById("modal");
+    if (modalElement?.classList.contains("hide")) {
+        modalElement?.classList.add("hide");
+    };
+
+    modalElement?.classList.toggle("hide");
+};
+export const putModalText = function (text: string) {
+    const textElement = document.getElementById("modalText") as HTMLElement;
+    textElement.textContent = text;
+};
