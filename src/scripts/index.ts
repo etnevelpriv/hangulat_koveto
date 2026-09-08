6f5dc39549a758662ac2fd2b98d3b3f668deeb78
etnevelpriv <leventerastovits@gmail.com>
2026-09-08 07:21:14 +0200

    html fajlok egy resze elkeszult, a scriptek betoltese keszen van.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,15 @@
-import { Create_Hangulat, Update_Hangulat, Read_Hangulat, Read_Hangulat_Osszes, Delete_Hangulat} from "./services/Hangulat_CRUD";
 const init = async function () {
-     
-}
-
+    const scriptElement = document.createElement("script");
+    if (document.getElementById("kezdooldalContainer")) {
+        scriptElement.src = "/src/scripts/kezdooldal.ts";
+        scriptElement.type = "module";
+    } else if (document.getElementById("hangulataimContainer")) {
+        scriptElement.src = "/src/scripts/hangulataim.ts";
+        scriptElement.type = "module";
+    } else if (document.getElementById("statisztikaContainer")) {
+        scriptElement.src = "/src/scripts/statisztika.ts";
+        scriptElement.type = "module";
+    }
+    document.body.appendChild(scriptElement);
+};
 document.addEventListener("DOMContentLoaded", init);
